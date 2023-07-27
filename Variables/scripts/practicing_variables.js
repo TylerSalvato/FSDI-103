1ebb6a7ff80be8579f523c4f74d9033198ebd9cf
TylerSalvato <133399190+TylerSalvato@users.noreply.github.com>
2023-07-27 18:29:38 -0400

    Add files via upload

---
+++ Variables/scripts/practicing_variables.js
@@ -0,0 +1,91 @@
+//Creating variables with let
+let myName = "Tyler";
+let myLastName = "Salvato";
+
+//Creating a numerical variable
+let myAge = 28;
+let mySize = 5.9;
+
+//Creating a boolean variable
+let isProfessor = false;
+let isStudent = true; 
+
+//Let's see the value
+console.log(myName);
+console.log(myLastName);
+console.log(myAge);
+console.log(mySize);
+console.log(isProfessor);
+console.log(isStudent);
+
+//Concatenate
+console.log(" My name is " + myName);
+console.log(" My name is " + myName + " I am " + myAge + " years old, and my size is " + mySize + "." );
+
+//Operations
+
+let num1 = 10;
+let num2 = 2;
+
+//Basic operations +, -, *, and /.
+let addition = num1 + num2;
+console.log(addition);
+let substraction = num1 - num2;
+let multiplication = num1 * num2; 
+let division = num1 / num2;
+console.log(substraction);
+console.log(division);
+
+//challenge
+let a = 3.14 * 9
+console.log(a);
+
+let radiusValue = 9;
+let areaValue = 28.26;
+
+console.log("The area of a circle with a radius of " + radiusValue + "cm is " + areaValue + "cm.");
+
+//correct
+let p = 3.1416;
+let radi = 3;
+
+let are = p * (radi * radi);
+
+console.log(" ")
+
+/*Constant
+const pi = 3.1416;
+let radius = prompt("Enter the radius: ");
+
+let area = pi * radius * radius;*/
+
+console.log(area);
+
+//difference
+
+let variableName;
+variableName = 30;
+
+console.log(variableName);
+
+const myFirstConst = 30; 
+
+variableName = 31;
+console.log(variableName);
+
+
+//use let or var
+
+var myFirstName = "Glenda";
+var myFirstName = "Mark";
+var myFirstName = "Fer";
+
+let myLastNam = "Rodrigez";
+
+document.write(" My name is " + myName + ", I am " + myAge + " years old, and my size is " + mySize + "." );
+
+//template string
+
+document.write(`<p>My name is ${myName}, ${myAge} years old and my size is ${mySize} </p>`)
+
+document.write(`vote; videos; games; shows; icons; images; arrows; new; popular; all; summer; puppy; nature; topics; video; titles; time; episode; space; feeling; dinosaurs;`);
---
+++ script.js
@@ -0,0 +1,27 @@
+//alert("Hello Students");
+console.error("Server error");
+console.warn("Sending private data")
+console.log("Server connection successful");
+
+let myVote = `Curious Campout`;
+let myVideos =  `Tower of Cows`;
+let myShows = `Farm Time Fun`;
+let myIcons = 974;
+let myImages = `Rosie Care`;
+let myArrows = `Pollinator Pathway`;
+let myNew = `Work it out Wombats`;
+let myPopular = `Daniel Tiger`;
+let myAll = `Sunshine`;
+let mySummer = `Wild Kratts`;
+let myPuppy = `Rocket`;
+let myNature = `Donkey Stand`;
+let myTopics = `Odd Squad`;
+let myVideo = `Magic Duck`;
+let myTitles = `Puppy Pet Care`;
+let myTime = `2:53`
+let myEpisode = `Push Pull Puzzle`;
+let mySpace = `Hero Maker`;
+let myFeeling = `Sandbox`;
+let myDinosaurs = `River Run`;
+
+document.write(`<p> ${myVote}, ${myVideos}, ${myShows}, ${myIcons}, ${myImages}, ${myArrows}, ${myNew}, ${myPopular}, ${myAll}, ${mySummer}, ${myPuppy}, ${myNature}, ${myTopics}, ${myVideo}, ${myTitles}, ${myTime}, ${myEpisode}, ${mySpace}, ${myFeeling}, ${myDinosaurs}. </p>`);
